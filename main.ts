432c5a138a8d1024e104959d5b6a49caf3c33e4d
AngadSC <104170661+AngadSC@users.noreply.github.com>
2022-05-31 14:48:12 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,3 +8,21 @@ basic.forever(function () {
     photorecpt_2 = pins.analogReadPin(AnalogPin.P2)
     pins.analogWritePin(AnalogPin.P14, photorecpt_2)
 })
+basic.forever(function () {
+    pins.servoWritePin(AnalogPin.P4, pins.map(
+    pins.analogReadPin(AnalogPin.P2),
+    0,
+    1023,
+    10,
+    170
+    ))
+})
+basic.forever(function () {
+    pins.servoWritePin(AnalogPin.P4, pins.map(
+    pins.analogReadPin(AnalogPin.P1),
+    0,
+    1023,
+    10,
+    170
+    ))
+})
